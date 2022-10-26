929552f6656f9408e4420a9f555efbd311b9a307
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-10-26 09:51:13 +0200

    first commit

---
+++ fld_01.py
@@ -0,0 +1,8 @@
+irsz = input('irányítószám: ')
+varos = input('település: ')
+kt_nev = input('közterület neve: ')
+kt_jell = input('közterület jellege: ')
+hsz = input('házszám: ')
+
+print(irsz + ' ' + varos + ', ' + kt_nev + ' ' + kt_jell + ' ' + hsz + '.')
+print(f'{irsz} {varos}, {kt_nev} {kt_jell} {hsz}.')
---
+++ fld_07.py
@@ -0,0 +1,6 @@
+a = int(input('derékszögű 3szög egyik befogója: '))
+b = int(input('derékszögű 3szög másik befogója: '))
+
+c = (a ** 2 + b ** 2) ** .5
+
+print(f'derékszögű háromszög átfogója {c} cm')
